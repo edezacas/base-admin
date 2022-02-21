17389698143d4f4d29a36f6c4803416a344fe817
eduarddeza <edezacas@gmail.com>
2022-02-21 18:39:26 +0100

    feat: Adding filepond

--- src/Form/Type/FilepondType.php
+++ src/Form/Type/FilepondType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FilepondType extends AbstractType
@@ -25,16 +27,29 @@ class FilepondType extends AbstractType
             ]);
     }
 
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['multiple'] = $options['multiple'];
+        $view->vars['uploadUri'] = $options['uploadUri'];
+        $view->vars['files'] = $options['files'];
+        $view->vars['acceptedFileTypes'] = $options['acceptedFileTypes'];
+    }
+
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'compound' => true,
             'multiple' => false,
-            'uploadUri' => ''
+            'uploadUri' => '',
+            'files' => [],
+            'acceptedFileTypes' => ''
         ]);
 
         $resolver->setAllowedTypes('uploadUri', 'string');
         $resolver->setAllowedTypes('multiple', 'bool');
+        $resolver->setAllowedTypes('files', 'array');
+        $resolver->setAllowedTypes('acceptedFileTypes', 'string');
     }
 
     public function getBlockPrefix(): string
